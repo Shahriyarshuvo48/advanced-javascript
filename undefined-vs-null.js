647fa730775ef243a4ffe4ed8cc778b4c782ffc3
Shahriyarshuvo48 <shahriyarshuvo0048@gmail.com>
2021-02-03 16:19:54 +0600

    null vs undefined coding

---
+++ undefined-vs-null.js
@@ -0,0 +1,12 @@
+// let name; no value set
+
+// function add(x,y){
+//     console.log(x+y); no return statement
+// }
+// const result = add(2,5);
+// const sam = {name: "cool dude",phone:1233546};
+// console.log(sam.gfName);
+// let fun = undefined;
+let age = [11,21,12];
+const result = age[11];
+console.log(result);
